0471afd94a68579abac129f486a7cb5a1b03de75
Leevi <joona.mlin@gmail.com>
2022-10-18 16:34:51 +0300

    Multiple layers working

--- src/index.js
+++ src/index.js
@@ -1,36 +1,66 @@
 import { Chart } from "../node_modules/frappe-charts/dist/frappe-charts.min.esm.js";
 
-function initializeMap(data) {
-    let map = L.map('map').setView([60.19, 24.94], 5);
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+function initializeMap(regdata, mundata) {
+    console.log(regdata);
+    let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    });
 
     let munLayer = L.geoJSON(null, {
         onEachFeature: (feature, layer) => {
             layer.bindTooltip(feature.properties.name + "<br/>Väkiluku: " + feature.properties.vaesto)
-            layer.on("click", () => { //hopefully correct syntax
+            layer.on("click", () => {
                 //TODO add data to chart 
                 console.log(feature.properties.name);
             }) 
         }
-    }).addTo(map);
-    data.forEach(element => {
+    });
+    mundata.forEach(element => {
         munLayer.addData(element);
     });
-    map.fitBounds(munLayer.getBounds());
 
+    let regLayer = L.geoJSON(null, {
+        onEachFeature: (feature, layer) => {
+            layer.bindTooltip(feature.properties.name + "<br/>Väkiluku: " + feature.properties.vaesto)
+            layer.on("click", () => { 
+                //TODO add data to chart 
+                console.log(feature.properties.name);
+            }) 
+        }
+    });
+    regdata.forEach(element => {
+        regLayer.addData(element);
+    });
+
+
+
+    let map = L.map('map', {
+        layers: [osm, munLayer, regLayer]
+    }).setView([60.19, 24.94], 5);
+
+    let baseMaps = {
+        "OpenStreetMap": osm
+    }
+
+    let overlayMaps = {
+        "Regions": regLayer,
+        "Municipalities": munLayer
+    }
+
+    let layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+    map.fitBounds(munLayer.getBounds());
 }
 
 async function getmapdata() {
     const municipalityurl = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=kunta_vaki2021&outputFormat=json&srsName=EPSG:4326";
     const regionurl = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=maakunta_vaki2021&outputFormat=json&srsName=EPSG:4326";
-    const data = await fetch(municipalityurl);
-    let jsonData = await data.json();
-    console.log(jsonData);
-    initializeMap(jsonData.features);
+    const regdata = await fetch(regionurl);
+    const regjson = await regdata.json();
+    const mundata = await fetch(municipalityurl);
+    const munjson = await mundata.json();
+    initializeMap(regjson.features, munjson.features);
 }
 
 function getUserSelection() {
